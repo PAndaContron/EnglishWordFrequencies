f6116271483002f2b1a6952378fe4da35d5aec47
ContronPAnda <rajtel99@gmail.com>
2020-05-16 19:04:32 -0400

    Added files

---
+++ process.py
@@ -0,0 +1,24 @@
+import sys
+import string
+
+words = {}
+
+for line in sys.stdin:
+    tokens = line.split('\t')
+    ngram, freq = tokens[0].lower(), int(tokens[2])
+    if '_' in ngram:
+        ngram = ngram[:ngram.index('_')]
+    alpha = True
+    for c in ngram:
+        if c not in string.ascii_lowercase:
+            alpha = False
+            break
+    if not alpha:
+        continue
+    if ngram in words.keys():
+        words[ngram] += freq
+    else:
+        words[ngram] = freq
+    
+for ngram in words:
+    print('%s,%d' % (ngram, words[ngram]))
